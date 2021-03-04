e83a2911c886e6d14d51d5a697612a4d1a079720
KGJ7 <glectechs16@gmail.com>
2021-03-04 22:24:34 +0000

    add

---
+++ src/sample/Controller.java
@@ -0,0 +1,36 @@
+package sample;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML
+    private Button closeButton;
+
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private TextField userInput;
+
+    @FXML
+    private ListView<String> fruitList;
+
+    @FXML
+    private void addItem(){
+        if(userInput != null){
+            fruitList.getItems().add(userInput.getText());
+            userInput.clear();
+        }
+    }
+
+    @FXML
+    private void closeWindow(){
+//        Platform.exit();
+        closeButton.getScene().getWindow().hide();
+
+    }
+}
